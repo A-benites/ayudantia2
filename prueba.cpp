1a322cac0b44bb7f8b93b831a8868831e845edb1
a-benites <amir.benites@alumnos.ucn.cl>
2026-08-27 17:31:10 -0400

    mensaje de prueba

---
+++ prueba.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amirb on 27-08-2026.
+//
+
+#include "prueba.h"
---
+++ prueba.h
@@ -0,0 +1,13 @@
+//
+// Created by amirb on 27-08-2026.
+//
+
+#ifndef AYUDANTIA1_PRUEBA_H
+#define AYUDANTIA1_PRUEBA_H
+
+
+class prueba {
+};
+
+
+#endif //AYUDANTIA1_PRUEBA_H
